1795485d7d372d379511910f8e02539ecc026902
viktoria-nedz <viktoria.nedzelska@gmail.com>
2024-12-08 19:52:09 +0200

    Added apiValues for temp,hum,speed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = "Sunday";
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
